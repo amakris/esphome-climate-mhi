6d2e68865dcdc193e1ec39f986abafffc289abbb
Alexandros Makris <makris.alexandros@gmail.com>
2025-11-29 23:01:16 +0200

    Update temperature constants to float type
    Changed temperature constants from uint8_t to float for better precision.

--- esphome/components/mhi/mhi.h
+++ esphome/components/mhi/mhi.h
@@ -1,26 +1,25 @@
 #pragma once
-#include <set>
 #include "esphome/components/climate_ir/climate_ir.h"
 
 namespace esphome {
     namespace mhi {
         // Temperature
-        const uint8_t MHI_TEMP_MIN = 18; // Celsius
-        const uint8_t MHI_TEMP_MAX = 30; // Celsius
+        const float MHI_TEMP_MIN = 18; // Celsius
+        const float MHI_TEMP_MAX = 30; // Celsius
 
         class MhiClimate : public climate_ir::ClimateIR {
             public:
                 MhiClimate() : climate_ir::ClimateIR(
                     MHI_TEMP_MIN, MHI_TEMP_MAX, 1.0f, true, true,
-                    std::set<climate::ClimateFanMode>{
+                    {
                         climate::CLIMATE_FAN_AUTO, climate::CLIMATE_FAN_LOW,
                         climate::CLIMATE_FAN_MEDIUM, climate::CLIMATE_FAN_HIGH
                     },
-                    std::set<climate::ClimateSwingMode>{
+                    {
                         climate::CLIMATE_SWING_OFF, climate::CLIMATE_SWING_VERTICAL,
                         climate::CLIMATE_SWING_HORIZONTAL, climate::CLIMATE_SWING_BOTH
                     },
-                    std::set<climate::ClimatePreset>{
+                    {
                         climate::CLIMATE_PRESET_NONE, climate::CLIMATE_PRESET_ECO,
                         climate::CLIMATE_PRESET_BOOST, climate::CLIMATE_PRESET_ACTIVITY,
                         climate::CLIMATE_PRESET_SLEEP
